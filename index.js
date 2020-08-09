b5c04732e83dcf5c899cd061dd7b06a114cfb0f3
uhFinn <38315554+uhFinn@users.noreply.github.com>
2020-08-09 21:04:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = (str) => {
+    if(typeof str == 'string'){
+        return true
+    } else {
+        return false
+    }
+}
+
+/*                                                                                                                                                                                                                                                            
+  iiii                                                    tttt                              iiii                                       
+ i::::i                                                ttt:::t                             i::::i                                      
+  iiii                                                 t:::::t                              iiii                                       
+                                                       t:::::t                                                                         
+iiiiiii     ssssssssss              ssssssssss   ttttttt:::::ttttttt   rrrrr   rrrrrrrrr  iiiiiiinnnn  nnnnnnnn       ggggggggg   ggggg
+i:::::i   ss::::::::::s           ss::::::::::s  t:::::::::::::::::t   r::::rrr:::::::::r i:::::in:::nn::::::::nn    g:::::::::ggg::::g
+ i::::i ss:::::::::::::s        ss:::::::::::::s t:::::::::::::::::t   r:::::::::::::::::r i::::in::::::::::::::nn  g:::::::::::::::::g
+ i::::i s::::::ssss:::::s       s::::::ssss:::::stttttt:::::::tttttt   rr::::::rrrrr::::::ri::::inn:::::::::::::::ng::::::ggggg::::::gg
+ i::::i  s:::::s  ssssss         s:::::s  ssssss       t:::::t          r:::::r     r:::::ri::::i  n:::::nnnn:::::ng:::::g     g:::::g 
+ i::::i    s::::::s                s::::::s            t:::::t          r:::::r     rrrrrrri::::i  n::::n    n::::ng:::::g     g:::::g 
+ i::::i       s::::::s                s::::::s         t:::::t          r:::::r            i::::i  n::::n    n::::ng:::::g     g:::::g 
+ i::::i ssssss   s:::::s        ssssss   s:::::s       t:::::t    ttttttr:::::r            i::::i  n::::n    n::::ng::::::g    g:::::g 
+i::::::is:::::ssss::::::s       s:::::ssss::::::s      t::::::tttt:::::tr:::::r           i::::::i n::::n    n::::ng:::::::ggggg:::::g 
+i::::::is::::::::::::::s        s::::::::::::::s       tt::::::::::::::tr:::::r           i::::::i n::::n    n::::n g::::::::::::::::g 
+i::::::i s:::::::::::ss          s:::::::::::ss          tt:::::::::::ttr:::::r           i::::::i n::::n    n::::n  gg::::::::::::::g 
+iiiiiiii  sssssssssss             sssssssssss              ttttttttttt  rrrrrrr           iiiiiiii nnnnnn    nnnnnn    gggggggg::::::g 
+                                                                                                                               g:::::g 
+                                                                                                                   gggggg      g:::::g 
+                                                                                                                   g:::::gg   gg:::::g 
+                                                                                                                    g::::::ggg:::::::g 
+                                                                                                                     gg:::::::::::::g  
+                                                                                                                       ggg::::::ggg    
+                                                                                                                          gggggg       
+*/
